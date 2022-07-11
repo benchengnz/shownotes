4638291640e483328e8f728d6cc942eb786e481d
ben cheng <nz_fox@hotmail.com>
2022-07-11 22:15:57 +1200

    initial page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header'; 
+import React, {useState} from 'react';
 
 function App() {
+  var options = [
+    { key: "1", text: "how are you", ctext: "你好嗎" },
+    { key: "2", text: "are you hungry", ctext: "你餓嗎" },
+    { key: "3", text: "do you want to go to toilet", ctext: "你想去廁所嗎" },
+    { key: "4", text: "feeling ok?", ctext: "你有沒不舒服嗎" },
+  ];
+  
+  const [text, setText] = useState("type something");
+  const [defaultfontsize, setFontSize] = useState(32);
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setText(event.target.value)
+  }
+
+  const handleIncreaseFont = () => {
+    setFontSize( defaultfontsize + 2)
+  }
+
+  const handleDecreaseFont = () => {
+    setFontSize(defaultfontsize - 2)
+  }
+
+  const handleSetText = (str) =>{
+    setText(str);
+  }
+
+  const textStyle = {
+    fontSize: defaultfontsize
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Heading'>
+        <Header selectedOption="1"
+          options={options}
+          setTextAction={handleSetText}
+          increaseFontSize={handleIncreaseFont}
+          descreaseFontSize={handleDecreaseFont}
+          title={Date().slice(4, 15)} />
+      </div>
+      <div className='Content'>
+        <div className='row'>
+          <div className='col'>
+            <textarea
+              style={textStyle}
+              className='mainText' onChange={handleInputChange}
+              value={text}></textarea>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,38 @@
+import { Button } from "react-bootstrap";
+import Form from 'react-bootstrap/Form'
+
+function Header(props) {
+
+    const handleOnChange = (event) => {
+        console.log(event.target.value.toString());
+        props.setTextAction(event.target.value.toString());
+    }
+
+    const handleClearText = (e) => {
+        props.setTextAction('');
+    }
+
+    return (
+        <div className="row" >
+            <div className="col-8">
+                <Form.Select onChange={handleOnChange} >{
+                    props.options.map(element => {
+                        return <option key={element.key}
+                            value={element.ctext}>{element.text}</option>;
+                    })}</Form.Select>
+            </div>
+            <div className="col-1" >
+                <button onClick={props.increaseFontSize} variant="outline-Info">➕</button>
+            </div>
+            <div className="col-1" >
+                <button onClick={props.descreaseFontSize} variant="outline-Info">➖</button>
+            </div>
+            <div className="col-1" >
+                <button onClick={handleClearText} variant="outline-danger">❌</button>
+            </div>
+        </div>
+
+    );
+}
+
+export default Header;
